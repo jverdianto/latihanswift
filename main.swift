13e6c935ce4fa022de411aa4a35daef67d87a284
Jeffrey8 <>
2022-03-08 01:46:39 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,79 @@
+// var temperature = 75
+
+// if (temperature >= 65) && (temperature <= 75) {
+//     print("Temperature is okay")
+// } else if temperature < 65{
+//     print("It is too cold")
+// } else {
+//   print("It is too hot")
+// }
+
+
+
+// var isRain = true
+
+// if isRain {
+//   print("Bring your umbrella")
+// }
+
+
+
+// var numberOfWheels = 2
+
+// switch numberOfWheels{
+//   case 0:
+//   print("Missing wheels")
+//   case 1:
+//   print("Unicycle")
+//   case 2:
+//   print("Bicycle")
+//   default:
+//   print("That's a lot of wheels")
+// }
+
+
+
+// let distance: Int = 25
+
+// switch distance {
+//   case 0...9:
+//   print("Your destination is close")
+//   case 10...99:
+//   print("Your destination is not that far")
+//   case 100...999:
+//   print("Your destination is far from here")
+//   default:
+//   print("Where are you going?")
+// }
+
+
+
+// print(Int8.max)
+// print(Int16.max)
+// print(Int32.max)
+// print(Int64.max)
+// print(Int.max)
+// print(Int.min)
+
+
+
+// let letter: Character = "u"
+
+// switch letter{
+//   case "a", "e", "i", "o", "u":
+//   print("vowel")
+//   default:
+//   print("not vowel")
+// }
+
+
+
+var largestNumber: Int
+
+let number1 = 40
+let number2 = 5
+
+//ternary operator
+largestNumber = (number1  > number2) ? number1 : number2
+
+print("the largest number is \(largestNumber)")
